b0c48d763a3c59ca8ed403f6f8c5b8c57db33df2
nidhisingh2004 <nidhikri20049@gmail.com>
2025-04-05 21:42:06 +0530

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar'
+import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
 
@@ -15,35 +17,53 @@ function App() {
   }
 
   const handleAdd = ()=>{
-    setTodos([...todos, {todo, isCompleted: false}])
+    setTodos([...todos, {id:uuidv4(),todo, isCompleted: false}])
     setTodo("")
+    console.log(todos)
   }
 
   const handleChange = (e)=>{
     setTodo(e.target.value)
   }
 
+  const handleCheckbox = (e) => {
+    let id= e.target.name;
+    console.log(id)
+    let index = todos.findIndex(item=>{
+      return item.id === id;
+    })
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos)
+  }
+  
+
 
   return (
     <>
     <Navbar/>
-    <div className='container mx-auto my-5 rounded-xl p-5 bg-slate-100 min-h-[80vh]'>
+    <div className='container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]'>
       <div className="addTodo my-5">
         <h2 className='text-lg font-bold'>Add a Todo</h2>
-        <input onChange={handleChange} value={todo} type="text" className='w-1/2' />
+        <input onChange={handleChange} value={todo} type="text" className='w-1/2 bg-white' />
         <button onClick={handleAdd} className='bg-slate-800 hover:bg-slate-950 p-3 py-1 text-white rounded-md mx-6'>Add</button>
       </div>
     <h2 className='text-xl font-bold'>Your Todos</h2>
     
     <div className="todos">
-    <div className="todo flex">
-    <div className="text">My Todo</div>
-    <div className='buttons'>
+      {todos.map(item=>{
+
+      
+    return <div key={item.id} className="todo flex w-1/4 my-3 justify-between">
+      <input name={item.id} onChange={handleCheckbox} type="checkbox" value={todo.isCompleted} id="" />
+    <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
+    <div className="buttons">
       <button onClick={handleEdit} className='bg-slate-800 hover:bg-slate-950 p-3 py-1 text-white rounded-md mx-2'>Edit</button>
-      <button onClick={handleDelete} className='bg-slate-800 hover:bg-slate-950 p-3 py-1 text-white rounded-md mx-2'>Delete</button>
+      <button onClick={()=>{handleDelete(item.id)}} className='bg-slate-800 hover:bg-slate-950 p-3 py-1 text-white rounded-md mx-2'>Delete</button>
 
     </div>
     </div>
+    })}
     </div>
     </div>
     </>
